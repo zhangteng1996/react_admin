b74c21a618e7134f0d27e3884469275434dfca78
xsd <xsd@qq.com>
2019-01-16 14:56:06 +0800

    NO.1

---
+++ src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+
+/*
+管理的Home路由组件
+ */
+export default class Home extends Component {
+  render() {
+    return (
+      <div>
+        Home
+      </div>
+    )
+  }
+}
