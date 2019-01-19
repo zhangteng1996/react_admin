3133f24970d2ff95ae305bc9218d8b6673607668
xsd <xsd@qq.com>
2019-01-19 10:11:41 +0800

    'NO.3'

--- src/api/index.js
+++ src/api/index.js
@@ -13,7 +13,68 @@ const BASE = 'http://localhost:5000'
 export const reqLogin = (username, password) => ajax('/login', {username, password}, 'POST')
 
 // 添加用户
-export const reqAddUser = (user) => ajax('/manage/user/add', user, 'POST')
+export const reqAddOrUpdateUser = (user) => ajax('/manage/user/'+(user.id ? 'update':'add'), user, 'POST')
+// 获取用户列表
+export const reqUsers = () => ajax('/manage/user/list')
+
+// 删除用户
+export const reqDeleteUser = (userId) => ajax('/manage/user/delete', {userId}, 'POST')
+
+
+// 获取一级或某个二级分类列表
+export const reqCategorys = (parentId = '0') => ajax('/manage/category/list', {parentId})
+
+// 添加分类
+export const reqAddCategory = (parentId, categoryName) => ajax('/manage/category/add', {
+    parentId,
+    categoryName
+}, 'POST')
+
+// 更新品类名称
+export const reqUpdateCategory = (categoryId, categoryName) => ajax('/manage/category/update', {
+    categoryId,
+    categoryName
+}, 'POST')
+
+
+// 根据分类ID获取分类
+export const reqCategory = (categoryId) => ajax('/manage/category/info', {categoryId})
+
+// 获取商品分页列表
+export const reqProducts = (pageNum, pageSize) => ajax('/manage/product/list', {pageNum, pageSize})
+
+// 根据ID/Name搜索产品分页列表
+export const reqSearchProducts = ({pageNum, pageSize, searchType, searchWord}) => ajax('/manage/product/search', {
+    pageNum,
+    pageSize,
+    [searchType]: searchWord,
+})
+
+// 添加/更新商品
+export const reqAddOrUpdateProduct = (product) => ajax('/manage/product/' + (product._id ? 'update' : 'add'), product, 'post')
+
+// 对商品进行上架/下架处理
+export const reqUpOrDownProduct = (productId, status) => ajax('/manage/product/updateStatus', {
+    productId,
+    status
+}, 'POST')
+
+// 删除图片
+export const deleteImg = (name) => ajax('/manage/img/delete', {name}, 'post')
+
+
+// 添加角色
+export const reqAddRole = (roleName) => ajax('/manage/role/add', {
+    roleName
+}, 'POST')
+
+// 获取角色列表
+export const reqRoles = () => ajax('/manage/role/list')
+
+// 更新角色(给角色设置权限)
+export const reqUpdateRoles = (role) => ajax('/manage/role/update', role, 'POST')
+
+
 
 // 请求获取天气
 export function reqWeather (city) {
